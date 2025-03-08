61fa57161f94a27c3c04828a6aaa79ee220ea950
Martinelii <Kevenmartineli@outlook.com>
2025-03-08 16:17:40 -0300

    feat: Class Usuario

---
+++ src/main/java/com/generation/cashFlow/model/Usuario.java
@@ -0,0 +1,73 @@
+package com.generation.cashFlow.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_usuarios")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank(message = "Campo com preenchimento Obrigatorio")
+	private String nome;
+
+	@NotBlank(message = "Campo Email é obrigatorio")
+	@Email(message = "Campo Email é obrigatorio")
+	private String email;
+	
+	@NotBlank(message = "Campo senha é Obrigatorio")
+	@Size(min = 8, message = "Senha deve conter no minimo 8 caracters")
+	private String senha;
+
+	public Usuario(Long id, String nome, String email, String senha) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+	}
+	
+	public Usuario() {	
+	}
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+}
